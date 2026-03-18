2a3d511f95a47020257f104e51ecad0d7eaee122
Amit-1424 <amit1424.work@gmail.com>
2026-03-18 21:35:51 +0530

    Backend setup:
    - Created 3-layer server (Express + HTTP + Socket.IO)
    - Setup MongoDB Atlas connection with Mongoose
    - Added environment variable support (.env) [planned/partial]
    - Added middlewares: CORS, JSON & URL-encoded parsing with size limits#

---
+++ backend/src/app.js
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import {createServer} from "node:http";
+import { Server } from "socket.io";
+
+import mongoose from "mongoose";
+import cors from "cors";
+
+import {connectToSocket} from "./controllers/socketManager.js"
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", (process.env.PORT || 8000  ) );
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb",extended: true}));
+
+app.get("/home",(req,res) => {
+    return res.json({"hello" : "world"})
+});
+
+const start = async () => {
+
+    const connectionDB  = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`mongo connected to DB host ${connectionDB.connection.host}`)
+    server.listen( app.get("port") ,() => {
+        console.log("listing on port 8000")
+    });
+};
+
+start();
