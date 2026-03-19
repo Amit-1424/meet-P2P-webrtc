24fd7d100c0f1efc44e08a523db19f83160c6997
Amit-1424 <amit1424.work@gmail.com>
2026-03-19 18:44:56 +0530

    added user model

---
+++ backend/src/models/users.models.js
@@ -0,0 +1,25 @@
+import {Schema} from "mongoose";
+
+const userSchema = new Schema ({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required : true,
+        unique : true,
+    },
+    password: {
+        type: String,
+        required : true,
+    },
+    token: {
+        type:string,
+    }
+    
+});
+
+const User = mongoose.model("User", userSchema);
+
+export { User };
